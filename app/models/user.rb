c8501d12cfd2a135fa7039223ad8181098229fe7
ridwan <ridwanediallo99@gmail.com>
2022-10-08 11:17:41 +0000

    Update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
     has_many :recipes
     has_many :foods
+    has_many :recipe_foods, through: :recipes
     # validates :name, presence: true
 end
