886a751a08d9a73e2a9684d656039430a628a4f3
ridwan <ridwanediallo99@gmail.com>
2022-10-08 18:12:01 +0000

    Update User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :recipes
-  has_many :foods
+  has_many :recipes, dependent: :destroy
+  has_many :foods, dependent: :destroy
   # validates :name, presence: true
 
-  has_many :recipes
-  has_many :foods
   has_many :recipe_foods, through: :recipes
   # validates :name, presence: true
 end
